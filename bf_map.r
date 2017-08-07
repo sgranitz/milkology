387e75d2f94db8be20552158d6847d11037fe28b
Stephan Granitz <sgranitz@gmail.com>
2017-08-06 21:56:37 -0400

    Add ever breastfed map

--- bf_map.r
+++ bf_map.r
@@ -21,12 +21,28 @@ library(scales)
 us <- usa_composite()
 us_map <- fortify(us, region="name")
 
-bf_data <- read_csv("C:/Users/Stephan/Desktop/cdc_bf_data.csv")
+url <- "https://raw.githubusercontent.com/sgranitz/milkology/master/cdc_bf_data.csv"
+bf_data <- as_tibble(read.csv(url, fileEncoding = "UTF-8-BOM"))
+
 bf_data <- bf_data %>% 
   mutate(grp = ifelse(exc_bf_6mth > 15, ifelse(exc_bf_6mth >= 25.5, "c. >=25.5%", "b. 15-25.5%"), "a. <15%"))
 bf_data$grp <- as.factor(bf_data$grp)
 
-bf_data$name <- bf_data$state
+bf_data <- bf_data %>% 
+  mutate(grp2 = ifelse(ever > 50, 
+                      ifelse(ever >= 60, 
+                             ifelse(ever >= 70,
+                                    ifelse(ever >= 80,
+                                           ifelse(ever >= 90, 
+                                                  "e. >= 90", 
+                                                  "d. >=80"),
+                                           "c. >= 70"),
+                                    "b. >= 60"),
+                             "a. >=50"),
+                      "<50"))
+bf_data$grp2 <- as.factor(bf_data$grp2)
+
+bf_data$name <- as.character(bf_data$state)
 us2 <- as_tibble(us) %>% left_join(bf_data, bf = "name")
 
 gg <- ggplot() + 
@@ -42,5 +58,15 @@ gg +
   coord_map() + labs(title = "Exclusive Breastfeeding Rates") +
   scale_fill_manual(name = "Per CDC 2016 Report: Exclusive Breastfeeding at 6 Months, %",
                     values = c("#EAB79E", "#D3DFE0", "#ECDBBC")) +
+  theme(legend.position = "bottom", 
+        legend.key.width = unit(3, "lines"))
+
+gg + 
+  geom_map(data = us@data, map = us_map,
+           aes(fill = us2$grp2, map_id = name),
+           color = "white", size = 0.1) +
+  coord_map() + labs(title = "Ever Breastfed Rates") +
+  scale_fill_manual(name = "Per CDC 2016 Report: Ever Breastfed, %",
+                    values = c("#EFFAFF", "#E4F0F2", "#D3DFE0", "#C2CDCF", "#B2BCC0")) +
   theme(legend.position = "bottom", 
         legend.key.width = unit(3, "lines"))
